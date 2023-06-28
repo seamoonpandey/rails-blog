a58013b29a502a30d217519bb9a6d4a905405209
Seamoon Pandey <hackedasocial@gmail.com>
2023-06-28 18:57:35 +0545

    sign in on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the rails blog #{@user.username}"
             redirect_to user_path(@user)
         else
